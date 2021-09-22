d7d44eca7dc44bddded587eb67d03acb1e971e75
SergioZ96 <sergiod.zurita@gmail.com>
2021-09-22 18:54:12 -0400

    added Colors and Titles enum types

---
+++ src/com.dollarsbank.utility/Colors.java
@@ -0,0 +1,11 @@
+package com.dollarsbank.utility;
+
+public enum Colors {
+    
+    ANSI_RESET("\u001B[0m"),
+    ANSI_BLUE("\u001B[34m"),
+    ANSI_GREEN("\u001B[32m"),
+    ANSI_CYAN("\u001B[36m"),
+    ANSI_RED("\u001B[31m");
+
+}
---
+++ src/com.dollarsbank.utility/Titles.java
@@ -0,0 +1,15 @@
+package com.dollarsbank.utility;
+
+public enum Titles {
+    
+    MAIN("DOLLARSBANK Welcomes You!"),
+    CREATE_ACCOUNT("Enter Details For New Account"),
+    LOGIN("Enter Login Details"),
+    SUCCESS_LOGIN("WELCOME CUSTOMER!!!"),
+    DEPOSIT("Deposit Amount"),
+    WITHDRAW("Withdraw Amount"),
+    FUNDS_TRANSFER("Transfer Funds"),
+    RECENT_TRANS("5 Recent Transactions"),
+    DISP_CUST_INFO("Display Customer Information");
+
+}
